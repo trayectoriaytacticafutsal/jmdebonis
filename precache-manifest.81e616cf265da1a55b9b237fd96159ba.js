5964841c654b9ad2f753ee411c4d8668e61bbb5e
Juan Pablo De Bonis <juan.p.debonis@gmail.com>
2020-05-05 22:36:45 -0300

    Updates

---
+++ precache-manifest.81e616cf265da1a55b9b237fd96159ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7bb1b6e6f55770809ed75d9f46bca9",
+    "url": "/jmdebonis/index.html"
+  },
+  {
+    "revision": "b02156f6e6633e8edaad",
+    "url": "/jmdebonis/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ee11e06adbe19198bf0a",
+    "url": "/jmdebonis/static/js/2.271db51c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jmdebonis/static/js/2.271db51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02156f6e6633e8edaad",
+    "url": "/jmdebonis/static/js/main.a2461571.chunk.js"
+  },
+  {
+    "revision": "6ab74266d58df63a1f03",
+    "url": "/jmdebonis/static/js/runtime-main.580a9b45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jmdebonis/static/media/logo.5d5d9eef.svg"
+  }
+]);
